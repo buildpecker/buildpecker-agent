038058be4bb1c7ac28b343ff4ac661f5b867984e
Parthajeet-Sarmah <coderparth32768@gmail.com>
2026-06-28 01:25:01 +0530

    command execution timeouts, no root running cmds

--- internal/deploy/infrahealth.go
+++ internal/deploy/infrahealth.go
@@ -1,24 +1,58 @@
 package deploy
 
 import (
+	"context"
 	"os/exec"
+	"strconv"
 	"strings"
+	"syscall"
+	"time"
+
+	"os/user"
 
 	"github.com/pthsarmah/buildpecker-agent/internal/api"
 	ctypes "github.com/pthsarmah/buildpecker-agent/types"
 	"github.com/pthsarmah/buildpecker-agent/utils"
 )
 
+const healthCommandTimeout = 30 * time.Second
+
+func unprivilegedCredential() (*syscall.Credential, error) {
+	u, err := user.Lookup("nobody")
+	if err != nil {
+		return nil, err
+	}
+	uid, err := strconv.Atoi(u.Uid)
+	if err != nil {
+		return nil, err
+	}
+	gid, err := strconv.Atoi(u.Gid)
+	if err != nil {
+		return nil, err
+	}
+	return &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)}, nil
+}
+
 func handleInfraHealth(target ctypes.InfraHealthTarget) {
 	logger, _ := utils.GetLoggerInstance()
 
 	project := composeProjectName(target.ContainerName, target.DeploymentId)
 
+	ctx, cancel := context.WithTimeout(context.Background(), healthCommandTimeout)
+	defer cancel()
+
 	var cmd *exec.Cmd
 	if strings.TrimSpace(target.Service) == "" {
-		cmd = exec.Command("sh", "-c", target.Command)
+		cred, err := unprivilegedCredential()
+		if err != nil {
+			logger.DeployLogger.Printf("Infra health dep=%s skipped: no unprivileged user for host command: %v", target.DeploymentId, err)
+			return
+		}
+		cmd = exec.CommandContext(ctx, "sh", "-c", target.Command)
+		cmd.SysProcAttr = &syscall.SysProcAttr{Credential: cred}
 	} else {
-		cmd = exec.Command(
+		cmd = exec.CommandContext(
+			ctx,
 			"docker", "compose", "-p", project,
 			"exec", "-T", target.Service,
 			"sh", "-c", target.Command,
--- internal/deploy/postinstall.go
+++ internal/deploy/postinstall.go
@@ -1,28 +1,37 @@
 package deploy
 
 import (
+	"context"
 	"os/exec"
+	"time"
 
 	"github.com/pthsarmah/buildpecker-agent/internal/api"
 	ctypes "github.com/pthsarmah/buildpecker-agent/types"
 	"github.com/pthsarmah/buildpecker-agent/utils"
 )
 
+const postInstallTimeout = 10 * time.Minute
+
 func handlePostInstall(run ctypes.PostInstallRun) {
 	logger, _ := utils.GetLoggerInstance()
+
+	ctx, cancel := context.WithTimeout(context.Background(), postInstallTimeout)
+	defer cancel()
 
 	var cmd *exec.Cmd
 	if run.Type == "project" {
 		container := deriveImageName(run.RepoUrl)
 		logger.DeployLogger.Printf("Postinstall run=%s name=%q container=%s", run.Id, run.Name, container)
-		cmd = exec.Command(
+		cmd = exec.CommandContext(
+			ctx,
 			"docker", "exec", "-i", container,
 			"sh", "-c", run.Command,
 		)
 	} else {
 		project := composeProjectName(run.ContainerName, run.DeploymentId)
 		logger.DeployLogger.Printf("Postinstall run=%s name=%q project=%s service=%s", run.Id, run.Name, project, run.Service)
-		cmd = exec.Command(
+		cmd = exec.CommandContext(
+			ctx,
 			"docker", "compose", "-p", project,
 			"exec", "-T", run.Service,
 			"sh", "-c", run.Command,
@@ -37,6 +46,9 @@ func handlePostInstall(run ctypes.PostInstallRun) {
 		} else {
 			exitCode = 1
 			out = append(out, []byte("\n"+err.Error())...)
+		}
+		if ctx.Err() == context.DeadlineExceeded {
+			out = append(out, []byte("\ncommand timed out after "+postInstallTimeout.String())...)
 		}
 	}
 
